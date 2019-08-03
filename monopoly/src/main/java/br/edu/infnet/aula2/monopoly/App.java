19a59532c287704a13033e1d2d931f995cc71059
DesenvolvedorJava <DesenvolvedorJava@192.168.154.130>
2019-08-03 11:06:12 -0300

    Comit inicial

---
+++ monopoly/src/main/java/br/edu/infnet/aula2/monopoly/App.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.aula2.monopoly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
